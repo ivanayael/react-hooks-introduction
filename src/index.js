ad01893b683d09aaf1dd70ea8e306563473a923b
Ivana Yael Curra <ivanayael@hotmail.com>
2020-05-12 15:34:02 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
+/* 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { counter: 0}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render () {
+    return (
+    <div>
+      <span>Counter Value: {this.state.counter}</span>
+      <button onClick={() => this.setState({ counter: this.state.counter + 1})} >
+        Increment Counter
+      </button>
+      <button onClick={() => this.setState({ counter: this.state.counter - 1})} >
+        Decrement Counter
+      </button>
+    </div>
+    )
+  }
+} */
+
+
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Counter() {
+  
+  const [count, setCount] = useState(0)
+return (
+    <div>
+      <span>Counter Value: {count} </span>
+      <button onClick={() => setCount(count + 1)}>
+        Increment Counter
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement Counter
+      </button>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
